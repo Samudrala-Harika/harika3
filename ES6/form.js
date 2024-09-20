2fcf37f85d27984e6bd019621a8571d503c43d65
Samudrala-Harika <harikasamudrala03@gmail.com>
2024-09-20 19:47:54 +0530

    some html and javascript programs on 19 and 20-09-24

---
+++ ES6/form.js
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <form>
+    Enter UserName:<input type="text" name="UserName"/><br/>
+    Entetr UserPassWord:<input type="password" name="password"/><br/>
+    
+        </form>
+    </body>
+</html>
